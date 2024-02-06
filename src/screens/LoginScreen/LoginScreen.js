22f9711e0a589365e235de26447e81042933ff8c
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-06 10:02:34 -0300

    Creating title component

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { Container } from '../../components/Container/style'
 import Logo from '../../components/Logo'
+import { Title } from '../../components/Title/style'
 
 export default function LoginScreen() {
   return (
     <Container>
       <Logo />
+      <Title>
+        Entrar ou criar conta
+      </Title>
     </Container>
   )
 }
