1da45b69627c386cfa829422c5210931cb8a8c80
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-06 09:55:16 -0300

    Creating logo component

--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Container } from '../../components/Container/style'
+import Logo from '../../components/Logo'
 
 export default function LoginScreen() {
   return (
     <Container>
-      <Text>LoginScreen</Text>
+      <Logo />
     </Container>
   )
 }
