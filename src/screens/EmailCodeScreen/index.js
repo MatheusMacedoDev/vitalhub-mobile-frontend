177982e1ab11537f1c7046de18dd5d6fcb3ce397
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-09 10:40:54 -0300

    Creating code input

--- src/screens/EmailCodeScreen/index.js
+++ src/screens/EmailCodeScreen/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from '../../components/Container/style';
 import Logo from '../../components/Logo';
 import { Title } from '../../components/Title/style';
@@ -6,26 +6,33 @@ import { CommandText, CommandTextHighlight } from '../../components/CommandText/
 import { UnsignedButtonsWrapper } from '../../components/UnsignedButton/style';
 import UnsignedButton from '../../components/UnsignedButton';
 import UnsignedLink from '../../components/UnsignedLink';
+import CodeInput from '../../components/CodeInput';
 
 export default function EmailCodeScreen() {
-  return (
-    <Container>
-        <Logo />
-        <Title>
-            Verifique seu e-mail
-        </Title>
-        <CommandText>
-            Digite o código de 4 dígitos enviado para
-            <CommandTextHighlight> username@email.com</CommandTextHighlight>
-        </CommandText>
-        <UnsignedButtonsWrapper>
-            <UnsignedButton 
-                buttonText='Entrar'
+    const [code, setCode] = useState('');
+
+    return (
+        <Container>
+            <Logo />
+            <Title>
+                Verifique seu e-mail
+            </Title>
+            <CommandText>
+                Digite o código de 4 dígitos enviado para
+                <CommandTextHighlight> username@email.com</CommandTextHighlight>
+            </CommandText>
+            <CodeInput 
+                code={code}
+                setCode={setCode}
             />
-        </UnsignedButtonsWrapper>
-        <UnsignedLink 
-            linkText='Reenviar Código'
-        />
-    </Container>
-  )
+            <UnsignedButtonsWrapper>
+                <UnsignedButton 
+                    buttonText='Entrar'
+                />
+            </UnsignedButtonsWrapper>
+            <UnsignedLink 
+                linkText='Reenviar Código'
+            />
+        </Container>
+    )
 }
