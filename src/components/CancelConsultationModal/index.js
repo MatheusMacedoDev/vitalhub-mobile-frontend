1357f6630e01ef96fac469419aaf725a9425c2a7
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-21 08:30:17 -0300

    Applying the modal component to CancelConsultationModal

--- src/components/CancelConsultationModal/index.js
+++ src/components/CancelConsultationModal/index.js
@@ -1,24 +1,22 @@
 import React from 'react'
-import { BlackBox, ModalContainer } from './style'
 import { Title } from '../Title/style';
 import { CommandText } from '../CommandText/style';
 import UnsignedButton from '../UnsignedButton';
 import UnsignedLink from '../UnsignedLink'
+import Modal from '../Modal';
 
 export default function CancelConsultationModal({ active, disableModalFn = null }) {
   return (
-    <BlackBox active={active}>
-        <ModalContainer active={active}>
-            <Title marginTop={0}>Cancelar consulta</Title>
-            <CommandText>Ao cancelar essa consulta, abrirá uma possível disponibilidade no seu horário, deseja mesmo cancelar essa consulta?</CommandText>
-            <UnsignedButton 
-              buttonText='Confirmar'
-            />
-            <UnsignedLink 
-              handleClickFn={disableModalFn}
-              linkText='Cancelar'
-            />
-        </ModalContainer>
-    </BlackBox>
+    <Modal active={active}>
+      <Title marginTop={0}>Cancelar consulta</Title>
+      <CommandText>Ao cancelar essa consulta, abrirá uma possível disponibilidade no seu horário, deseja mesmo cancelar essa consulta?</CommandText>
+      <UnsignedButton 
+        buttonText='Confirmar'
+      />
+      <UnsignedLink 
+        handleClickFn={disableModalFn}
+        linkText='Cancelar'
+      />
+    </Modal>
   )
 }
