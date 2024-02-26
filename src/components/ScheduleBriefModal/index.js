f5a74778910e7ec83efdce945eaa0c6fe1656b9a
Matheus Macedo <matheus.macedo.developer@gmail.com>
2024-02-26 10:52:38 -0300

    Starting to create schedule brief modal

---
+++ src/components/ScheduleBriefModal/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Modal from '../Modal';
+
+export default function ScheduleConsultationModal() {
+  return (
+    <Modal>
+        
+    </Modal>
+  )
+}
